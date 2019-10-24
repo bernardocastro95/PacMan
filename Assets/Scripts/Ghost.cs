8a7d8a21fea3969e99a6adecbc44c645349f680e
Bernardo <bernardoc544@gmail.com>
2019-10-23 23:15:32 -0400

    Adding the ghost's script

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public Transform[] wayPoints;
+    private int cur = 0;
+    public float speed = 0.3f;
+
+    void FixedUpdate() 
+    {
+        if(transform.position.x != wayPoints[cur].position.x || transform.position.y != wayPoints[cur].position.y)
+        {
+            Vector2 p = Vector2.MoveTowards(transform.position, wayPoints[cur].position, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        else 
+        {
+            cur = (cur + 1) % wayPoints.Length;
+        }    
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.name == "pacman")
+        {
+            Destroy(other.gameObject);
+        }  
+    }
+}
